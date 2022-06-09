a2eec81131f37d14a1df3d986f578b861d9fbe0f
8BitJonny <christiansen.timon@gmail.com>
2022-06-09 00:26:35 -0400

    :tada: (core): first working mvp
    adds message handler, shows tokenLeaderboard, handles tokenSending

--- index.js
+++ index.js
@@ -1,19 +1,13 @@
 const { App } = require('@slack/bolt')
+const Airtable = require('airtable');
 const store = require('./store')
 const messages = require('./messages')
 const helpers = require('./helpers')
+const dayjs = require('dayjs')
+var weekOfYear = require('dayjs/plugin/weekOfYear')
+dayjs.extend(weekOfYear)
 
 require('dotenv').config()
-const userMentionDetectionRegex = /<@(.*)>/
-const containsUserMention = (string) => userMentionDetectionRegex.test(string)
-const extractUsersFromString = (string) => [...string.matchAll(userMentionDetectionRegex)].map(e => e[1])
-const countNoodlesInMessage = (string) => [...string.matchAll(/(:good-noodle:)/g)].length
-const addEmoji = (app, context, message, emoji) => app.client.reactions.add({
-  token: context.botToken,
-  name: emoji,
-  channel: message.channel,
-  timestamp: message.ts
-});
 
 const app = new App({
   signingSecret: process.env.GOODNOODLE_SLACK_SIGNING_SECRET,
@@ -22,10 +16,90 @@ const app = new App({
   logLevel: 'DEBUG'
 });
 
+const WEEKLY_TOKEN_AMOUNT = 5
+
+const airtable = new Airtable({ apiKey: process.env.AIRTABLE_KEY }).base('appF6p1Lb1oJR01eN')
+const userTable = airtable('Users')
+const sendTokensTable = airtable('TokensSend')
+
+const userMentionDetectionRegex = /<@(.*)>/
+const containsUserMention = (string) => userMentionDetectionRegex.test(string)
+const extractUsersFromString = (string) => [...string.matchAll(/<@(.{11})>/g)].map(e => e[1])
+const countNoodlesInMessage = (string) => [...string.matchAll(/(:good-noodle:)/g)].length
+const addEmoji = (app, context, message, emoji) => app.client.reactions.add({
+  token: context.botToken,
+  name: emoji,
+  channel: message.channel,
+  timestamp: message.ts
+});
+
+async function getUser(userID) {
+  return (await userTable
+    .select({ maxRecords: 1, filterByFormula: `{UserID} = '${userID}'` })
+    .all())[0]
+}
+async function getRemainingWeeklyTokens(userID) {
+  const allSendTransactions = await sendTokensTable
+    .select({ filterByFormula: `AND({Week} = ${dayjs().week()}, {Year} = ${dayjs().year()}, {SenderID} = '${userID}')` })
+    .all()
+  const sendTokenAmount = allSendTransactions.reduce((sum, record) => sum + record.get('TokenAmount'), 0)
+  return sendTokenAmount >= 0
+    ? WEEKLY_TOKEN_AMOUNT - sendTokenAmount
+    : 0
+}
+function fetchName(userID) {
+  return app.client.users.profile.get({ user: userID })
+    .then(response => {
+      if (!response.ok) {
+        Promise.reject(response.error)
+      }
+      return response.profile.display_name || response.profile.real_name
+    })
+}
+async function upsertUsers(userIDs) {
+  return Promise.all(userIDs.map(async (userID) => {
+    const userExists = await getUser(userID)
+    if (userExists) {
+      return userExists.id
+    } else {
+      const userName = await fetchName(userID)
+      return (await insertUser(userID, userName))[0].id
+    }
+  }))
+}
+async function getUsers() {
+  return await userTable
+    .select()
+    .all()
+}
+function insertUser(userID, firstName) {
+  return userTable
+    .create([{
+      fields: {
+        UserID: userID,
+        "First Name": firstName,
+      }
+    }])
+}
+function writeSendNoodlesToDB(transaction) {
+  // Transaction length should not exceed 10 per Airtable API
+  return sendTokensTable.create(
+    transaction.map(({ sender, recipient, tokenAmount: TokenAmount }) => ({
+      fields: {
+        Week: dayjs().week(),
+        Year: dayjs().year(),
+        TokenAmount,
+        Recipient: [recipient],
+        Sender: [sender]
+      }
+    }))
+  )
+}
+
 app.event('app_home_opened', async ({ event, say }) => {
-  console.log('1')
-  let user = store.getUser(event.user);
-  console.log('2')
+  const users = await getUsers();
+  const user = users.find((u) => u.get('UserID') === event.user)
+  const tokensLeft = await getRemainingWeeklyTokens(event.user)
 
   // Call views.publish with the built-in client
   const result = await app.client.views.publish({
@@ -37,13 +111,40 @@ app.event('app_home_opened', async ({ event, say }) => {
         "type": "section",
         "text": {
           "type": "mrkdwn",
-          "text": "*Welcome home, <@" + event.user + "> :house:*"
+          "text": "*Welcome, <@" + event.user + "> :house:*"
         }
+      }, {
+        "type": "header",
+        "text": {
+          "type": "plain_text",
+          "text": "My Noodles"
+        }
+      }, {
+        "type": "divider"
       }, {
         "type": "section",
         "text": {
           "type": "mrkdwn",
-          "text": "Learn how home tabs can be more useful and interactive <https://api.slack.com/surfaces/tabs/using|*in the documentation*>."
+          "text": `Received: ${user.get('TokensReceived')} :good-noodle:\n\nGiven: ${user.get('TokensSend')} :good-noodle:\n\nLeft to Give this week: ${tokensLeft} :good-noodle:`
+        }
+      }, {
+        "type": "header",
+        "text": {
+          "type": "plain_text",
+          "text": "Noodle Received Leaderboard"
+        }
+      }, {
+        "type": "divider"
+      }, {
+        "type": "section",
+        "text": {
+          "type": "mrkdwn",
+          "text": users
+            .sort((a, b) => b.get('TokensReceived') - a.get('TokensReceived'))
+            .map((u, i) => {
+              return `*${i+1}.) <@${u.get('UserID')}>:* ${':good-noodle:'.repeat(u.get('TokensReceived'))} (${u.get('TokensReceived')})`
+            })
+            .join('\n\n')
         }
       }]
     }
@@ -51,21 +152,34 @@ app.event('app_home_opened', async ({ event, say }) => {
 });
 
 app.message(':good-noodle:', async ({ message, context, say }) => {
-  console.log(message)
-
   if (!containsUserMention(message.text)) return;
 
   const mentionedUsers = extractUsersFromString(message.text);
   const giftedNoodles = countNoodlesInMessage(message.text);
   console.log({ mentionedUsers, giftedNoodles })
+
+  const [sender, ...recipients] = await upsertUsers([
+    message.user,
+    ...mentionedUsers
+      .filter(u => u !== message.user)
+  ])
+  
+  const totalTokensToBeSend = recipients.length * giftedNoodles
+  const tokensLeft = await getRemainingWeeklyTokens(message.user)
+  if (tokensLeft - totalTokensToBeSend < 0) {
+    await addEmoji(app, context, message, 'eyes');
+    return
+  }
+
+  await writeSendNoodlesToDB(recipients.map(recipient => ({ sender, recipient, tokenAmount: giftedNoodles })))
   
   const result = await addEmoji(app, context, message, 'thumbsup');
 })
 
 app.error(console.error);
 
-// Start your app
 (async () => {
+  console.log(process.env.PORT || 3000)
   await app.start(process.env.PORT || 3000);
   console.log('⚡️ Bolt app is running!');
 
